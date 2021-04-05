ea33f2bb93667cd8a1bfc094e432e85734a1cbb1
NS H <starsurfer2010@gmail.com>
2021-04-05 11:17:23 -0700

    Rendering Goals
    - updated UI so that goals from database are shown
    - wrote pad function to pad numbers in the streak flame

--- 5-FinalProduct/app/js/db.js
+++ 5-FinalProduct/app/js/db.js
@@ -2,10 +2,10 @@
 db.collection("goals").onSnapshot((snapshot) => {
     //console.log(snapshot.docChanges());
     snapshot.docChanges().forEach(change => {
-        //console.log(change, change.doc.data(), change.doc.id);
+        console.log(change, change.doc.data(), change.doc.id);
         if(change.type === 'added') {
             // add the document data to the web page
-            //renderGoal(change.doc.data(), change.doc.id);
+            renderGoal(change.doc.data(), change.doc.id);
         }
     })
 })
--- 5-FinalProduct/app/js/ui.js
+++ 5-FinalProduct/app/js/ui.js
@@ -1,10 +1,46 @@
+const main_goals = document.querySelector('.main-goals');
+
 document.addEventListener('DOMContentLoaded', function() {
   // nav menu
   const menus = document.querySelectorAll('.side-menu');
   M.Sidenav.init(menus, {edge: 'right'});
   // add recipe form
-  const forms = document.querySelectorAll('.side-form');
-  M.Sidenav.init(forms, {edge: 'left'});
+  //const forms = document.querySelectorAll('.side-form');
+  //M.Sidenav.init(forms, {edge: 'left'});
 });
 
 // render goal data
+const renderGoal = (data, id) => {
+  
+  let current_streak = pad(data.current_streak, 3);
+  let check_box_status = (data.checked == true) ? "check_box" : "check_box_outline_blank";
+  
+  const html = `
+    <div class="card-panel white row valign-wrapper" data-id="${id}">
+        <div class="col s3 left-align">
+          <i class="material-icons medium" data-id="${id}">${check_box_status}</i>
+        </div>
+        <div class="col s7">
+          <div class="goal-title">${data.name}</div>
+        </div>
+        <div class="col s2 right-align">
+          <div class="streak-flame-small">
+            <img src="/img/flame.png" height="65" width="auto">
+            <p>${current_streak}</p>
+          </div>
+        </div>
+    </div>
+  `;
+
+  main_goals.innerHTML += html;
+}
+
+const pad = (input, maxLen) => {
+  //console.log(input.toString(10), input.toString(10).length);
+  let output = "";
+  for (let spaces = input.toString(10).length; spaces < maxLen; spaces += 1) {
+    output += "&nbsp;";
+  }
+  output += input;
+  return output;
+}
